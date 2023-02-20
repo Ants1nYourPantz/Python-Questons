e314ab38034cc9971919b495e8ae7ded1d658a12
Anthony Romano <dentlabpa@gmail.com>
2023-02-19 20:33:46 -0500

    questions

---
+++ python.py
@@ -0,0 +1,34 @@
+    # """QUESTION 1"""
+def USERNAME():
+    USERNAME = input("Enter your USERNAME:")
+    print("Hello, " + USERNAME)
+
+    # """QUESTION 2"""
+def first_odds():
+    for number in range(1,100):
+        if number % 2 == 1:
+            print(number)
+
+    """QUESTION 3"""
+def max_num_in_(a_list):
+    list = [99999999]
+    x = max(list)
+    print(x)
+
+# """QUESTION 4"""
+def is_leap_year(a_year):
+    a_year = (1)
+    if (a_year) % 4 == 0 and (a_year) != 100:
+        elif (a_year) % 400:
+    else (a_year) + 1
+    continue
+    print(True)
+
+# """QUESTION 5"""
+ def is_consecutive(a_list):
+    number = [1, 2, 3, 4]
+    for numbers in number + 1:
+        print(number)
+
+
+
